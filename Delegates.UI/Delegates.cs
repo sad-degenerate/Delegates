84fb2b697ce03e1227a76b9304890cd0e398cd27
two_down <mister.franz02@gmail.com>
2020-03-21 21:10:54 +0700

    Добавьте файлы проекта.

---
+++ Delegates.UI/Delegates.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Delegates.UI
+{
+    public delegate double Operation(double arg);
+
+    public class Delegates
+    {
+        public Operation[] GetDelegates(int count, int seed)
+        {
+            var operations = new Operation[count];
+
+            var rand = new Random(seed);
+
+            for(int i = 0; i < count; i++)
+            {
+                switch(rand.Next(0, 4))
+                {
+                    case 0:
+                        operations[i] = Add;
+                        break;
+
+                    case 1:
+                        operations[i] = Mult;
+                        break;
+
+                    case 2:
+                        operations[i] = Min;
+                        break;
+
+                    case 3:
+                        operations[i] = DivBy2;
+                        break;
+
+                    case 4:
+                        operations[i] = MultBy2;
+                        break;
+                }
+            }
+
+            return operations;
+        }
+
+        private double Add(double arg)
+        {
+            return arg + 10;
+        }
+
+        private double Mult(double arg)
+        {
+            return arg * 10;
+        }
+
+        private double Min(double arg)
+        {
+            return arg - 10;
+        }
+
+        private double DivBy2(double arg)
+        {
+            return arg / 2;
+        }
+
+        private double MultBy2(double arg)
+        {
+            return arg * 2;
+        }
+    }
+}
---
+++ Delegates.UI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Delegates.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var del = new Delegates();
+
+            for (int j = 0; j < 10; j++)
+            {
+                double numb = 44;
+                
+                var count = 10;
+                var operations = del.GetDelegates(count, j);
+
+                for (int i = 0; i < count; i++)
+                    numb = operations[i](numb);
+
+                Console.WriteLine("{0}-ый ответ: {1}", j, numb);
+            }
+        }
+    }
+}
